b5a0a2ba1f4b9f9c427c07612490928a94af2a6e
markJasonMargallo <201910681@gordoncollege.edu.ph>
2023-04-04 13:39:44 +0800

    add chained middleware

--- myApp.js
+++ myApp.js
@@ -2,29 +2,36 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
-app.use('/public', express.static(__dirname+'/public'))
+app.use('/public', express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
-  console.log(req.method+" "+req.url+" - "+req.ip)
+  console.log(req.method + " " + req.url + " - " + req.ip)
   next()
 })
 
-console.log(__dirname+'/public')
+console.log(__dirname + '/public')
 
 app.get('/json', (req, res) => {
 
   const message = "Hello json"
   const messageStyle = process.env.MESSAGE_STYLE
-  const modifiedMessage = messageStyle === "uppercase"? message.toUpperCase() : message
-  
-  res.json({message: modifiedMessage})
-  
-  })
+  const modifiedMessage = messageStyle === "uppercase" ? message.toUpperCase() : message
+
+  res.json({ message: modifiedMessage })
+
+})
+
+app.get('/now', function (req, res, next) {
+  req.time = new Date().toString()
+  next();
+}, function (req, res) {
+  res.json({ time: req.time })
+});
 
 app.get('/', (req, res) => {
-    file = __dirname + '/views/index.html'
-    res.sendFile(file)
-  })
+  file = __dirname + '/views/index.html'
+  res.sendFile(file)
+})
 
 
 
@@ -61,4 +68,4 @@ app.get('/', (req, res) => {
 
 
 
- module.exports = app;
+module.exports = app;
