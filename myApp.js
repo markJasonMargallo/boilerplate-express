2215c1aa0a5fb6e7f13e3dbb2c907a718a20579f
markJasonMargallo <201910681@gordoncollege.edu.ph>
2023-04-04 10:59:34 +0800

    add json route

--- myApp.js
+++ myApp.js
@@ -5,6 +5,10 @@ app.use('/public', express.static(__dirname+'/public'))
 
 console.log(__dirname+'/public')
 
+app.get('/json', (req, res) => {
+    res.json({message: "Hello json"})
+  })
+
 app.get('/', (req, res) => {
     file = __dirname + '/views/index.html'
     res.sendFile(file)
